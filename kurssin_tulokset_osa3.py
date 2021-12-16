39ef2e67f185337e5123ff87166dc307b7ba534e
Kasperi <95344893+ksprjarvi@users.noreply.github.com>
2021-12-16 17:14:59 +0200

    Update kurssin_tulokset_osa3.py

--- kurssin_tulokset_osa3.py
+++ kurssin_tulokset_osa3.py
@@ -80,7 +80,18 @@ def kurssin_tulokset():
                 for arvosana in osat[1:]:
                     kpisteet[osat[0]].append(int(arvosana))  # Lisätään arvosanat sanakirjaan.
     
-     # Tulostaa kaikki tiedot. 
+    # tulosta_otsikot() tulostaa kaikki yhdistetyt tiedot.
+    # Tulostusesimerkki:
+    # nimi                          teht_lkm  teht_pist koe_pist  yht_pist  arvosana  
+    # pekka peloton                 25        6         11        17        1         
+    # jaana javanainen              27        6         10        16        1         
+    # liisa virtanen                35        8         6         14        0         
+    # donald frump                  0         0         15        15        1         
+    # john doe                      28        7         16        23        3         
+    # angela tarkel                 32        8         13        21        3         
+    # karkki eila                   30        7         7         14        0         
+    # alan turing                   28        7         19        26        4         
+    # ada lovelace                  27        6         27        33        5   
     tulosta_otsikot()
     for opnro, nimi in opiskelijat.items():
         if opnro in tehtavat:
