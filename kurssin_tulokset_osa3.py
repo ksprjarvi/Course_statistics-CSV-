9b5251eb20972443868df00eb92d76b1add51e45
kj <kasperi.jarvi@gmail.com>
2021-12-16 17:07:52 +0200

    added kurssin_tulokset_osa3.py

---
+++ kurssin_tulokset_osa3.py
@@ -0,0 +1,98 @@
+# Funktio antaa lopullisen kurssin arvosanan riippuen pistemäärästä. 
+def lopullinen_arvosana(pisteet: int):
+    pisterajat = [15, 18, 21, 24, 28]
+    if pisteet >= 28:
+        return 5
+    for raja in pisterajat:
+        if pisteet < raja:
+            arvosana = pisterajat.index(raja)
+            return arvosana
+
+# Funktio luo ja palauttaa tehtävien arvosanan. 
+def tehtava_pisteet(tehtavat: int):
+    return tehtavat // 4
+
+# Funktio tulostaa otsikot oikeaan tulostusmuotoon. 
+def tulosta_otsikot():
+    nimi = 'nimi'
+    teht_lkm = 'teht_lkm'
+    teht_pist = 'teht_pist'
+    koe_pist = 'koe_pist'
+    yht_pist = 'yht_pist'
+    arvo_sana = 'arvosana'
+
+    print(f'{nimi:30}{teht_lkm:10}{teht_pist:10}{koe_pist:10}{yht_pist:10}{arvo_sana:10}')
+
+
+# Funktio lukee 3 CSV tiedostoa ja muodostaa niistä tulostettavan kokonaistilaston. 
+def kurssin_tulokset():
+    # Luetaan ensiksi valitut tiedostot.
+    opiskelijatiedot = input("Opiskelijatiedot: ")
+    tehtavatiedot = input("Tehtävätiedot: ")
+    koepisteet = input('Koepisteet: ')
+
+    # Lisätään opiskelijat omaan sanakirjaansa muodossa: opiskelijanumero, nimi
+    with open(opiskelijatiedot) as tiedosto:
+        opiskelijat = {}
+
+        # Käydään tiedosto läpi rivi riviltä.
+        for rivi in tiedosto:
+            rivi = rivi.replace('\n', '')
+            osat = rivi.split(';')  # Jaetaan rivi ; - merkkien kohdalta omiksi alkioikseen jotka lisätään 'osat' - listaan. 
+
+            # Käydään osat - lista läpi.
+            for osa in osat:
+                if osat[0] == 'opnro':  # Jos otsikkorivi; jatketaan.
+                    continue
+                opiskelijat[osat[0]] = str(f'{osat[1]} {osat[2]}')  # Lisätään opiskelijat sanakirjaan.
+    
+    # Lisätään tehtävät omaan sanakirjaansa muodossa: opiskelijanumero, tehtävät.
+    with open(tehtavatiedot) as tiedosto:
+        tehtavat = {}
+
+        # Käydään tiedosto läpi rivi riviltä. 
+        for rivi in tiedosto:
+            rivi = rivi.replace('\n', '') 
+            osat = rivi.split(';')  # Jaetaan rivi ; - merkkien kohdalta omiksi alkioikseen jotka lisätään 'osat' - listaan. 
+
+            # Käydään osat - lista läpi. 
+            for osa in osat: 
+                if osat[0] == 'opnro': # Jos otsikkorivi; jatketaan. 
+                    continue
+                tehtavat[osat[0]] = []
+                for tehtava in osat[1:]:
+                    tehtavat[osat[0]].append(int(tehtava))  # Lisätään tehtävät sanakirjaan. 
+    
+    # Lisätään koepisteet omaan sanakirjaansa muodossa: opiskelijanumero, arvosanat
+    with open(koepisteet) as tiedosto:
+        kpisteet = {}
+
+        # Käydään tiedosto läpi rivi riviltä. 
+        for rivi in tiedosto:
+            rivi = rivi.replace('\n', '')
+            osat = rivi.split(';')  # Jaetaan rivi ; - merkkien kohdalta omiksi alkioikseen jotka lisätään 'osat' - listaan.
+
+             # Käydään osat - lista läpi.
+            for osa in osat:
+                if osat[0] == 'opnro':  # Jos otsikkorivi; jatketaan. 
+                    continue
+                kpisteet[osat[0]] = []
+                for arvosana in osat[1:]:
+                    kpisteet[osat[0]].append(int(arvosana))  # Lisätään arvosanat sanakirjaan.
+    
+     # Tulostaa kaikki tiedot. 
+    tulosta_otsikot()
+    for opnro, nimi in opiskelijat.items():
+        if opnro in tehtavat:
+            if opnro in kpisteet:
+                pisteet_yhteensa = sum(kpisteet[opnro]) + tehtava_pisteet(sum(tehtavat[opnro]))  # Yhteispisteet
+                tulostettava_arvosana = lopullinen_arvosana(pisteet_yhteensa)  # Arvosana
+                tehtavat_lkm = sum(tehtavat[opnro])  # Tehtävien lukumäärä
+                tehtavien_pisteet = tehtava_pisteet(sum(tehtavat[opnro]))  # Tehtävien pisteet
+                koepisteet = sum(kpisteet[opnro])  # Koepisteet
+                print(f'{nimi:30}{tehtavat_lkm:<10}{tehtavien_pisteet:<10}{koepisteet:<10}{pisteet_yhteensa:<10}{tulostettava_arvosana:<10}')
+
+    
+if __name__ == '__main__':
+    pass
+kurssin_tulokset()
